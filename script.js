6f9b868aaaca74c7bc91f8e1d57cd74c98f316cd
rohansadhu91 <rohansadhu@yahoo.co.in>
2023-08-02 02:41:17 +0530

    FaceBook Clone--2nd commit

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-let settingsMenu= document.querySelector(".settings-menu")
-let darkBtn= document.getElementById("dark-btn");
+let settingsMenu = document.querySelector(".settings-menu");
+let darkBtn = document.getElementById("dark-btn");
 
-function settingsMenuToggle(){
-    settingsMenu.classList.toggle("settings-menu-height");
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
 }
 
-darkBtn.onclick=function(){
-    darkBtn.classList.toggle("dark-btn-on");
-    document.body.classList.toggle("dark-theme");
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
 
-   if(localStorage.getItem("theme") == "light"){
+  if (localStorage.getItem("theme") == "light") {
     localStorage.setItem("theme", "dark");
-   }
-   else{
-    localStorage.setItem("theme","light");
-}
-}
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
 
 // localStorage ues:-
 // takes 2 values (valueName, value)----> localStorage.setItem("theme","dark");
 // gives the value (valueName)---> localStorage.getItem("theme");
 
-if(localStorage.getItem("theme") == "light"){
-    darkBtn.classList.remove("dark-btn-on");
-    document.body.classList.remove("dark-theme");
-}
-else if(localStorage.getItem("theme") == "dark"){
-    darkBtn.classList.add("dark-btn-on");
-    document.body.classList.add("dark-theme");
-}
-else{
-    localStorage.setItem("theme","light");
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
 }
